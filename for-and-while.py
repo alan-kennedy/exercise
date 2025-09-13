0eba9a37309a7ab508225fbb76cd40accf83e3d2
alan-kennedy <alanpardblue1@gmail.com>
2025-09-13 00:30:02 -0600

    learn how to create a for and while loops

---
+++ for-and-while.py
@@ -0,0 +1,25 @@
+# Example of a for loop in Python
+""" total = 0
+for i in range(1, 11):
+    total = total + i
+    # 1 = 0 + 1
+    # 3 = 1 + 2
+    # 6 = 3 + 3
+    # 10 = 6 + 4
+    # 15 = 10 + 5
+    # 21 = 15 + 6
+    # 28 = 21 + 7
+    # 36 = 28 + 8
+    # 45 = 36 + 9
+    # 55 = 45 + 10
+    print(total) """
+
+# Example of a while loop in Python
+
+# print Warning while writing a number 8
+n = 8
+while (n < 100):
+    n = n + 10
+    print('Warning: ', n)
+
+    
